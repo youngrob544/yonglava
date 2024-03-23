f18414ee0178b5943bc95d5dc5de90e1a8c8d027
young rob <100993691+youngrob544@users.noreply.github.com>
2024-03-23 13:00:25 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://RPCETHLAVA/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-3921e533-e3fb-4d01-ae39-9c46d3fecdcb/)
 
 
 echo "$curl_output" >> Update.tsx
